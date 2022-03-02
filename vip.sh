2aa899a178c9c27d39f24afefe725b3139df9a0f
Mr.Pei <95112952+Xiaopei147@users.noreply.github.com>
2022-03-02 14:24:15 +0800

    Add files via upload

---
+++ vip.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+ip_c=`/sbin/ip a |grep "scope global eth0" |wc -l`
+status=`/sbin/service xxx status |grep "running"|wc -l`
+stop=`/sbin/service xxx status |grep "stopped" |wc -l`
+date=`/bin/date`
+pid=`ps -ef |grep xxx |grep -v grep |awk '{print $2}'`
+if [ $ip_c -eq 2 ]
+then 
+ #/sbin/service xxx status >>/tmp/vip.log
+ if [ $status -ne 1 ]
+ then
+ echo "========" >>/tmp/vip.log
+ echo "服务重启" >>/tmp/vip.log
+ echo $date >>/tmp/vip.log
+ echo "ip数量是:$ip_c" >>/tmp/vip.log 
+ echo "服务状态是:$status(1.start 0.no start)" >>/tmp/vip.log
+ /bin/kill  $pid >>/tmp/vip.log 2>&1
+ rm -rf /var/run/xxx.pid >>/tmp/vip.log
+ /sbin/service xxx restart >>/tmp/vip.log
+  if [ $status -ne 1 ]
+  then
+  /bin/bash vip.sh;
+  else
+  echo "service restart"
+  fi
+ else
+ echo "service is ok" >>/dev/null
+ fi
+else
+ if [ $stop -ne 1 ] 
+ then 
+  echo "========" >>/tmp/vip.log
+  echo "服务关闭" >>/tmp/vip.log
+  echo $date >>/tmp/vip.log
+  echo "ip数量是:$ip_c" >>/tmp/vip.log
+  echo "服务状态是:$status(1.start 0.no start)" >>/tmp/vip.log
+  /sbin/service xxx stop >>/tmp/vip.log
+ else
+  echo "" >/dev/null
+fi
+fi
