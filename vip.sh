872d88f91e014aee4e99d164b11d5dabfdd7ad34
Mr.Pei <95112952+Xiaopei147@users.noreply.github.com>
2022-03-02 14:29:10 +0800

    Update vip.sh

--- vip.sh
+++ vip.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#create user github:Xiaopei147
+#create date 2022/2/28 08:00:00(CST)
+#####################################################
+##⚠⚠⚠⚠⚠⚠⚠⚠The copyright of this script is mine##
+##⚠⚠⚠⚠⚠⚠⚠⚠      这个脚本的版权是我              ##
+#####################################################
 ip_c=`/sbin/ip a |grep "scope global eth0" |wc -l`
 status=`/sbin/service xxx status |grep "running"|wc -l`
 stop=`/sbin/service xxx status |grep "stopped" |wc -l`
